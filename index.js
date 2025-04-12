cc8b9af99c4bd0abaac0f6e2d5a946dd4e5507b1
Diwahar <diwadiwan1998@gmail.com>
2025-04-12 12:06:37 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+ var sidenav = document.querySelector(".side-navbar");
+
+ function openSlideBar()
+ {
+sidenav.style.left="0"
+    
+ }
+ function closeSlideBar()
+ {
+    sidenav.style.left="-60%"
+        
+     }
+
+     
